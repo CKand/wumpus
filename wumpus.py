dc90cb1508dafb054086c3b8c7e3be741f565ae1
Christine Kandalaft <christine.kandalaft@gmail.com>
2017-11-12 00:01:09 -0500

    Update wumpus.py

--- wumpus.py
+++ wumpus.py
@@ -1,21 +1,14 @@
 from utils import *
 from logic import *
 from agents import *
+from agent import *
 from state import *
 
-# print(wumpus_kb.clauses)
-
-#print("test");
-#puzzle = WumpusEnvironment(None,4,4);
-#print(puzzle.get_world());
-#print("im so lost");
-#print(puzzle.things);
 class WumpusWorld(State): 
 
 	def __init__(self, agent, worldState):
 		self.agent = agent;
 		self.worldState = worldState;
-		print("send help");
 
 
     # State is changed according to action
@@ -41,7 +34,7 @@ class WumpusWorld(State):
 		return [];
 	 # Returns a list of sensations from the current state
 	def percept(self, agent): 
-		return self.worldState.precept(self.agent);
+		return self.worldState.percept(self.agent);
 
 	#get agents location
 	def getCurrentLocation():
@@ -51,19 +44,24 @@ class WumpusWorld(State):
 	def getCurrentDirection():
 		return self.agent.direction;
 
-
-wumpus_kb = PropKB()
-P11, P12, P21, P22, P31, B11, B21 = expr('P11, P12, P21, P22, P31, B11, B21')
-wumpus_kb.tell(~P11)
-wumpus_kb.tell(B11 | '<=>' | ((P12 | P21)))
-wumpus_kb.tell(B21 | '<=>' | ((P11 | P22 | P31)))
-wumpus_kb.tell(~B11)
-wumpus_kb.tell(B21)
 #puzzle = WumpusEnvironment(None,4,4);
 #program here needs to be be done, it should take in precepts and return an action.
-agent = Agent(program); 
+#agent = Agent(program)
+agentTest = KnowledgeBasedAgent()
+#print(agentTest.choose_location(agentTest))
+
 #WumpusWorld(puzzle.get_world, [1,1],0, puzzle.percepts_from(puzzle.agents[0],(1,1)));#should just be puzzle.precept(agent) if had a valid agent right nwo 
-WumpusWorld(agent, WumpusEnvironment(None,4,4)); #where None should be agent
-print("uhh");
-TraceAgent(agent);
-print(agent)
+puzzle = WumpusWorld(agentTest, WumpusEnvironment(agentTest,4,4)); #where None should be agent
+
+print(puzzle.worldState.get_world())
+listper =puzzle.percept(agentTest)
+agent = Agent(agentTest.program)
+# print("here is the issue")
+# print(puzzle.percept(agentTest))
+#TraceAgent(agent);
+#print(agent)
+
+
+
+while not puzzle.worldState.is_done():
+	puzzle.worldState.execute_action(agentTest, agentTest.program(puzzle.percept(agentTest)))
